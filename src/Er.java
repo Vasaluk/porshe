b376883e9afefe4f16832e0182d92dbc6b08c5b2
Vasaluk <vasaluk24@gamil.com>
2023-04-27 00:32:56 +0300

    ddddd

--- src/Er.java
+++ src/Er.java
@@ -8,14 +8,4 @@ public class Er {
         System.out.println("AAAAAA");
         System.out.println("WWWWWWW");
     }
-
-    public void wwe() {
-        System.out.println("AAAAAA");
-        System.out.println("WWWWWWW");
-    }
-
-    public void mm() {
-        System.out.println("AAAAAA");
-        System.out.println("WWWWWWW");
-    }
 }
